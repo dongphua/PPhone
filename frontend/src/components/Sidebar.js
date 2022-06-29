ba5922441e960e21df21552a271dfe2acad757cf
dongphua <dongphuaa@gmail.com>
2022-06-29 15:44:46 +0700

    [PP-08] Fix permission frontend github

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import SidebarItem from "./SidebarItem"
+import items from "./DataSidebar.json"
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function Sidebar(){
+    return (
+        <div className="sidebar">
+            <div><h1>ADMIN</h1></div>
+          { items.map((item, index) => <SidebarItem key={index} item={item} />) }
+        </div>
+    )
+}
